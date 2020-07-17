3423e7436756d2311652e1b6d6de36b6fd5b911f
Márcio Florindo <61458507+marciojpflorindo@users.noreply.github.com>
2020-07-17 08:46:06 +0100

    Color change, changed from JQuery to vanilla JS

--- java.js
+++ java.js
@@ -1,36 +1,34 @@
-// a esplicação completa da matematica por trás das contas para fazer andar os ponteiros neste site https://css-tricks.com/css3-clock/
-
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-// Ponteiro dos segundos
-  setInterval(function() {
+  // Seconds hand
+  setInterval(function () {
     var seconds = new Date().getSeconds();
     var sDegree = seconds * 6;
     var sRotate = "rotate(" + sDegree + "deg)";
-    $(".ponteiro-segundos").css({"transform": sRotate});
-    $(".ponteiro-segundos").css({"transform-origin": "bottom"});
+    $(".seconds").css({ "transform": sRotate });
+    $(".seconds").css({ "transform-origin": "bottom" });
   }, 1000);
 
 
-  // Ponteiro das horas
-  setInterval(function() {
+  // Hours hand
+  setInterval(function () {
     var hours = new Date().getHours();
     var min = new Date().getMinutes();
     var hDegree = hours * 30 + (min / 2);
     var hRotate = "rotate(" + hDegree + "deg)";
-    $(".ponteiro-horas").css({"transform": hRotate});
-    $(".ponteiro-horas").css({"transform-origin": "bottom"});
+    $(".hour").css({ "transform": hRotate });
+    $(".hour").css({ "transform-origin": "bottom" });
   }, 1000);
 
 
-  // Ponteiro das minutos
-  setInterval(function() {
+  // Minutes hand
+  setInterval(function () {
     var minutes = new Date().getMinutes();
     var mDegree = minutes * 6;
     var mRotate = "rotate(" + mDegree + "deg)";
-    $(".ponteiro-minutos").css({"transform": mRotate});
-    $(".ponteiro-minutos").css({"transform-origin": "bottom"});
+    $(".minutes").css({ "transform": mRotate });
+    $(".minutes").css({ "transform-origin": "bottom" });
   }, 1000);
 
-})
+});
